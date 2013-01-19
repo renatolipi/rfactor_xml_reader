a5f82c098ce0722314e461a85bdd84e9cab93d8e
Renato Lipi <renatolipi@gmail.com>
2013-01-19 17:46:08 -0200

    Changing xml_reader to become object-oriented. Getting basic qualifying data.

--- core/xml_reader.py
+++ core/xml_reader.py
@@ -1,12 +1,27 @@
 #coding:utf-8
 
-from bs4 import BeautifulSoup
-
-def openXML(input_file):
-    xml_file = BeautifulSoup(open(input_file))
-    return xml_file
-
-def get_qualy_circuit(xml_file):
-    return xml_file.raceresults.trackcourse.string
+import sys
+from bs4 import BeautifulSoup as BS
 
 
+class QualyFile(object):
+
+    def __init__(self, input_file):
+        try:
+            self.xml_file = BS(open(input_file))
+        except IOError:
+            print "An IO error has occurred. Are you shure this file exists? Check if it is in this given path."
+            sys.exit()
+
+        self.__set_initial_info()
+
+    def __set_initial_info(self):
+        self.track_name = self.xml_file.trackcourse.string
+        self.track_length = self.xml_file.tracklength.string
+
+        self.race_laps = '{} laps'.format(self.xml_file.racelaps.string)
+        self.race_time = '{} min'.format(self.xml_file.racetime.string)
+        if self.xml_file.racelaps.string == '0':
+            self.race_length = self.race_time
+        else:
+            self.race_length = self.race_laps
--- rfactor_xml_log_reader.py
+++ rfactor_xml_log_reader.py
@@ -3,13 +3,16 @@
 from core import xml_reader
 import sys
 
-def dump_data(args):
-    xml_file = xml_reader.openXML(args[0])
-    qualy_circuit = xml_reader.get_qualy_circuit(xml_file)
-    print qualy_circuit
 
+def demonstration(args):
+    xml_file = xml_reader.QualyFile(args[0])
+    print 'QualyFile'
+    print 'Circuit: {}'.format(xml_file.track_name)
+    print 'Length: {}m'.format(xml_file.track_length)
+    print 'Laps: {}'.format(xml_file.race_laps)
+    print 'Time: {}'.format(xml_file.race_time)
+    print 'Race length: {}'.format(xml_file.race_length)
 
 
 if __name__ == '__main__':
-    print '---'
-    dump_data(sys.argv[1:])
+    demonstration(sys.argv[1:])
