77f2dcdd686634c7a04fe9618f17426fef5debf9
Renato Lipi <renatolipi@gmail.com>
2013-03-30 08:41:14 -0300

    Initial test sample.

--- test_module/test_sample.py
+++ test_module/test_sample.py
@@ -2,10 +2,19 @@
 
 import unittest
 
-class testSample(unittest.TestCase):
+from ..core.time_utils import prettify_laptime
+
+
+#running tests: python -m rfactor_xml_reader.test_module.test_sample
+
+
+class testUtils(unittest.TestCase):
 
     def test_sample(self):
-        self.assertEqual((1+1), 2)
+        '''just an example on prettify_laptime'''
+        pl_result = prettify_laptime(123)
+        self.assertEqual(pl_result, '02\'3"0')
+
 
 if __name__ == '__main__':
     unittest.main()
